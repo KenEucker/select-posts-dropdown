f5746f60bafebda6ab286c749499a8feb3101694
Ken Eucker <keneucker@gmail.com>
2019-05-20 14:28:06 -0700

    Added a temporary fix for individual fetches not grab ALL posts ahead of this component doing fetching the same post data.

--- src/SelectPostsDropdown.js
+++ src/SelectPostsDropdown.js
@@ -15,129 +15,131 @@ import apiFetch from '@wordpress/api-fetch'
 const styleLink = document.createElement('link')
 styleLink.rel = 'stylesheet'
 styleLink.href =
-    'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css'
+	'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css'
 document.head.append(styleLink)
 
 export default class SelectPostsDropdown extends React.Component {
-    static defaultProps = {
-        restBase: '/wp-json/wp/v2',
-        placeholder: 'Select Post',
-        fields: ['id', 'title'],
-        postType: 'posts',
-        saveToWpData: true,
-    }
+	static defaultProps = {
+		restBase: '/wp-json/wp/v2',
+		placeholder: 'Select Post',
+		fields: ['id', 'title'],
+		postType: 'posts',
+		saveToWpData: true,
+	}
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            showing_modal: false,
-            options: null,
-            currentValue: props.selectedValue
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			showing_modal: false,
+			options: null,
+			currentValue: props.selectedValue
+		}
+	}
 
-    handleAddition(e, { value }) {
-        this.setState(prevState => ({
-            options: [{ text: value, value }, ...prevState.options]
-        }));
-    }
+	handleAddition(e, { value }) {
+		this.setState(prevState => ({
+			options: [{ text: value, value }, ...prevState.options]
+		}));
+	}
 
-    handleChange(e, el) {
-        if (typeof this.props.limit !== 'undefined') {
-            if (el.value.length <= this.props.limit) {
-                this.setState({ currentValue: el.value });
-            } else {
-                return
-            }
-        }
+	handleChange(e, el) {
+		if (typeof this.props.limit !== 'undefined') {
+			if (el.value.length <= this.props.limit) {
+				this.setState({ currentValue: el.value });
+			} else {
+				return
+			}
+		}
 
-        let selected = null;
+		let selected = null;
 
-        if (this.props.multiple || Array.isArray(el.value)) {
-            selected = el.options.filter(opt => { return el.value.indexOf(opt.value) !== -1 })
-        } else {
-            selected = el.options.filter(opt => { return opt.value == el.value })
-        }
+		if (this.props.multiple || Array.isArray(el.value)) {
+			selected = el.options.filter(opt => { return el.value.indexOf(opt.value) !== -1 })
+		} else {
+			selected = el.options.filter(opt => { return opt.value == el.value })
+		}
 
-        if (!!selected && !!this.props.onChange) {
-            // TODO: Use the code below to grab more data requested by the user
-            // const includeFields = this.props.fields
+		if (!!selected && !!this.props.onChange) {
+			// TODO: Use the code below to grab more data requested by the user
+			// const includeFields = this.props.fields
 
-            // // only return the data the user wants
-            // const postData = Object.keys( selected )
-            // 	.filter( k => includeFields.includes( k ) )
-            // 	.map( k => Object.assign( {}, { [ k ]: selected[ k ] } ) )
-            // 	.reduce( ( res, o ) => Object.assign( res, o ), {} )
+			// // only return the data the user wants
+			// const postData = Object.keys( selected )
+			// 	.filter( k => includeFields.includes( k ) )
+			// 	.map( k => Object.assign( {}, { [ k ]: selected[ k ] } ) )
+			// 	.reduce( ( res, o ) => Object.assign( res, o ), {} )
 
-            selected = selected.map((v) => {
-                return { id: v.value, title: v.text }
-            })
+			selected = selected.map((v) => {
+				return { id: v.value, title: v.text }
+			})
 
-            if (this.props.multiple) {
-                this.props.onChange(selected)
-            } else if (selected && selected.length) {
-                this.props.onChange(selected[0])
-            }
-        }
-    }
+			if (this.props.multiple) {
+				this.props.onChange(selected)
+			} else if (selected && selected.length) {
+				this.props.onChange(selected[0])
+			}
+		}
+	}
 
-    componentDidMount() {
-        const savingToWpData = !!this.props.saveToWpData
-        const wpDataField = typeof this.props.saveToWpData === 'string' ? this.props.saveToWpData : 'wpData'
-        const additionalApiParams = this.props.per_page ? `per_page=${this.props.per_page}` : null;
+	componentDidMount() {
+		const savingToWpData = !!this.props.saveToWpData
+		const wpDataField = typeof this.props.saveToWpData === 'string' ? this.props.saveToWpData : 'wpData'
+		const additionalApiParams = this.props.per_page ? `per_page=${this.props.per_page}` : null;
 
-        const decodeHtmlText = function (str) {
-            return str.replace(/&#(\d+);/g, function (match, dec) {
-                return String.fromCharCode(dec)
-            })
-        }
+		const decodeHtmlText = function (str) {
+			return str.replace(/&#(\d+);/g, function (match, dec) {
+				return String.fromCharCode(dec)
+			})
+		}
 
-        const setOptsAndGlobalPostsFromPosts = (posts) => {
-            if (savingToWpData) {
-                window[wpDataField] = window[wpDataField] || {}
-                window[wpDataField][this.props.postType] = posts
-            }
+		const setOptsAndGlobalPostsFromPosts = (posts) => {
+			if (savingToWpData) {
+				window[wpDataField] = window[wpDataField] || {}
+				window[wpDataField][this.props.postType] = posts
+			}
 
-            const options = []
-            for (const post of posts) {
-                options.push({ text: decodeHtmlText(post.title.rendered), value: post.id })
-            }
+			const options = []
+			for (const post of posts) {
+				options.push({ text: decodeHtmlText(post.title.rendered), value: post.id })
+			}
 
-            this.setState({ options, posts })
-        }
+			this.setState({ options, posts })
+		}
 
-        if (!!window[wpDataField]
-            && !!window[wpDataField][this.props.postType]
-            && window[wpDataField][this.props.postType].length) {
-            setOptsAndGlobalPostsFromPosts(window[wpDataField][this.props.postType])
-        } else {
-            apiFetch({ path: `${this.props.restBase}/${this.props.postType}?${additionalApiParams}` })
-                .then((posts) => {
-                    setOptsAndGlobalPostsFromPosts(posts)
-                })
-        }
-    }
+		// TODO: Refactor this because individual fetches won't grab ALL posts
+		if (!!window[wpDataField]
+			&& !!window[wpDataField][this.props.postType]
+			&& !!window[wpDataField][this.props.postType].length
+			&& window[wpDataField][this.props.postType].length > 1) {
+			setOptsAndGlobalPostsFromPosts(window[wpDataField][this.props.postType])
+		} else {
+			apiFetch({ path: `${this.props.restBase}/${this.props.postType}?${additionalApiParams}` })
+				.then((posts) => {
+					setOptsAndGlobalPostsFromPosts(posts)
+				})
+		}
+	}
 
-    render() {
-        const { className, heading, multiple, placeholder } = this.props
-        const { options } = this.state
-        const { currentValue } = this.state;
+	render() {
+		const { className, heading, multiple, placeholder } = this.props
+		const { options } = this.state
+		const { currentValue } = this.state;
 
-        return (
-            <div className={className}>
-                <span>{heading}</span>
-                <Dropdown
-                    placeholder={placeholder}
-                    onAddItem={this.handleAddition.bind(this)}
-                    onChange={this.handleChange.bind(this)}
-                    fluid
-                    search
-                    selection
-                    multiple={multiple}
-                    options={options}
-                    value={currentValue}
-                />
-            </div>
-        )
-    }
+		return (
+			<div className={className}>
+				<span>{heading}</span>
+				<Dropdown
+					placeholder={placeholder}
+					onAddItem={this.handleAddition.bind(this)}
+					onChange={this.handleChange.bind(this)}
+					fluid
+					search
+					selection
+					multiple={multiple}
+					options={options}
+					value={currentValue}
+				/>
+			</div>
+		)
+	}
 }
